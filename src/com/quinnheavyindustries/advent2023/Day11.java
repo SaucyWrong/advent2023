a8bc11a847487d05de61b9fef8f752ea2df3ef85
Matt Quinn <matt@quinnheavyindustries.com>
2023-12-11 16:23:05 -0500

    day 11 part 2 complete

--- src/com/quinnheavyindustries/advent2023/Day11.java
+++ src/com/quinnheavyindustries/advent2023/Day11.java
@@ -9,102 +9,135 @@ import static java.lang.System.out;
 
 public class Day11 {
 
-    static LinkedList<Character> dots(int length) {
-        return ".".repeat(length).chars().mapToObj(c -> (char) c)
+    static String SPACE_CHAR = "O";
+    static int EXPANSION_DISTANCE = 1000000;
+
+    static LinkedList<Character> vastAmountsOfSpace(int length) {
+        return SPACE_CHAR.repeat(length).chars().mapToObj(c -> (char) c)
                 .collect(LinkedList::new, LinkedList::add, LinkedList::addAll);
     }
 
     public static void main(String[] args) {
-        LinkedList<LinkedList<Character>> galaxyChart = loadGalaxyChart();
-        out.println("Unexpended galaxy chart:\n" + charsToString(galaxyChart));
-
-        expandGalaxyChart(galaxyChart);
-        out.println("Expanded galaxy chart:\n" + charsToString(galaxyChart));
-
-        var allGalaxies = locateGalaxies(galaxyChart);
-        var uniqueGalaxyPairs = findGalaxyPairs(allGalaxies);
-        out.printf("Found %d galaxies and %d unique galaxy pairs\n", allGalaxies.size(), uniqueGalaxyPairs.size());
-
-        var solution = uniqueGalaxyPairs.stream()
-                .mapToLong(GalaxyPair::distance)
-                .sum();
-        out.printf("\nSolution: %d\n", solution);
+        var galaxyChart = new GalaxyChart(Utils.loadLines("day11-puzzle"));
+        out.printf("Solution: %d\n", galaxyChart.sumDistanceBetweenUniqueGalaxyPairs());
     }
 
-    static LinkedList<LinkedList<Character>> loadGalaxyChart() {
-        LinkedList<LinkedList<Character>> unexpandedGalaxy = Utils.loadLines("day11-puzzle").stream()
-                .map(line -> new LinkedList<>(line.chars().mapToObj(c -> (char) c).toList()))
-                .collect(LinkedList::new, LinkedList::add, LinkedList::addAll);
-        out.printf("Loaded unexpanded galaxy chart with %d rows and %d columns\n",
-                unexpandedGalaxy.size(), unexpandedGalaxy.getFirst().size());
-        return unexpandedGalaxy;
-    }
+    static class GalaxyChart {
+        LinkedList<LinkedList<Character>> chart;
+        List<Point> galaxyCoordinates;
+        Set<GalaxyPair> galaxyPairs;
+        SortedSet<Integer> expansionColumns = new TreeSet<>();
+        SortedSet<Integer> expansionRows = new TreeSet<>();
 
-    static void expandGalaxyChart(LinkedList<LinkedList<Character>> unexpandedGalaxy) {
-        var expandColumns = new ArrayList<Integer>();
-        var expandRows = new ArrayList<Integer>();
-        for (var x = 0; x < unexpandedGalaxy.getFirst().size(); x++) {
-            int finalX = x;
-            if (unexpandedGalaxy.stream().map(row -> row.get(finalX)).allMatch(c -> c == '.')) {
-                expandColumns.add(finalX);
-            }
-        }
-        for (var y = 0; y < unexpandedGalaxy.size(); y++) {
-            if (unexpandedGalaxy.get(y).stream().allMatch(c -> c == '.')) {
-                expandRows.add(y);
-            }
+        GalaxyChart(List<String> input) {
+            chart = input.stream()
+                    .map(line -> new LinkedList<>(line.chars().mapToObj(c -> (char) c).toList()))
+                    .collect(LinkedList::new, LinkedList::add, LinkedList::addAll);
+            out.printf("Loaded unexpanded galaxy chart with %d rows and %d columns\n",
+                    chart.size(), chart.getFirst().size());
+            out.println("Unexpended galaxy chart:\n" + charsToString(chart));
+
+            expandGalaxyChart();
+            out.println("Expanded galaxy chart:\n" + charsToString(chart));
+
+            galaxyCoordinates = locateGalaxies();
+            galaxyPairs = findGalaxyPairs();
+            out.printf("Found %d galaxies and %d unique galaxy pairs\n", galaxyCoordinates.size(), galaxyPairs.size());
         }
 
-        out.printf("Expansion required in columns %s and rows %s\n", expandColumns, expandRows);
-        var rowLength = unexpandedGalaxy.getFirst().size();
-        expandRows.reversed().forEach(row -> unexpandedGalaxy.add(row, dots(rowLength)));
-        expandColumns.reversed().forEach(column -> unexpandedGalaxy.forEach(row -> row.add(column, '.')));
-    }
-
-    static List<Point> locateGalaxies(LinkedList<LinkedList<Character>> galaxyChart) {
-        var galaxies = new ArrayList<Point>();
-        for (var y = 0; y < galaxyChart.size(); y++) {
-            for (var x = 0; x < galaxyChart.get(y).size(); x++) {
-                if (galaxyChart.get(y).get(x) == '#') {
-                    out.printf("Found galaxy at %d, %d\n", x, y);
-                    galaxies.add(new Point(x, y));
+        private void expandGalaxyChart() {
+            for (var x = 0; x < chart.getFirst().size(); x++) {
+                int finalX = x;
+                if (chart.stream().map(row -> row.get(finalX)).allMatch(c -> c == '.')) {
+                    expansionColumns.add(finalX);
                 }
             }
-        }
-        return galaxies;
-    }
-
-    static Set<GalaxyPair> findGalaxyPairs(List<Point> galaxies) {
-        var galaxyPairs = new HashSet<GalaxyPair>();
-        for (var i = 0; i < galaxies.size(); i++) {
-            for (var j = i + 1; j < galaxies.size(); j++) {
-                galaxyPairs.add(new GalaxyPair(galaxies.get(i), galaxies.get(j)));
+            for (var y = 0; y < chart.size(); y++) {
+                if (chart.get(y).stream().allMatch(c -> c == '.')) {
+                    expansionRows.add(y);
+                }
             }
+
+            out.printf("Expansion required in columns %s and rows %s\n", expansionRows, expansionColumns);
+            var rowLength = chart.getFirst().size();
+            expansionRows.reversed().forEach(row -> {
+                chart.add(row, vastAmountsOfSpace(rowLength));
+                chart.remove(row + 1);
+            });
+            expansionColumns.reversed().forEach(column -> {
+                chart.forEach(row -> row.add(column, SPACE_CHAR.charAt(0)));
+                chart.forEach(row -> row.remove(column + 1));
+            });
         }
-        return galaxyPairs;
-    }
 
-    static class GalaxyPair {
-        TreeSet<Point> coordinates = new TreeSet<>();
-
-        GalaxyPair(Point galaxyA, Point galaxyB) {
-            coordinates.add(galaxyA);
-            coordinates.add(galaxyB);
-        }
-
-        public long distance() {
-            if (coordinates.isEmpty() || coordinates.size() == 1) {
-                return 0;
+        private List<Point> locateGalaxies() {
+            var galaxies = new ArrayList<Point>();
+            for (var y = 0; y < chart.size(); y++) {
+                for (var x = 0; x < chart.get(y).size(); x++) {
+                    if (chart.get(y).get(x) == '#') {
+                        out.printf("Found galaxy at %d, %d\n", x, y);
+                        galaxies.add(new Point(x, y));
+                    }
+                }
             }
-            return coordinates.first().gridDistanceTo(coordinates.last());
+            return galaxies;
         }
 
-        @Override
-        public String toString() {
-            final StringBuilder sb = new StringBuilder("GalaxyPair{");
-            sb.append("coordinates=").append(coordinates);
-            sb.append('}');
-            return sb.toString();
+        private Set<GalaxyPair> findGalaxyPairs() {
+            var galaxyPairs = new HashSet<GalaxyPair>();
+            for (var i = 0; i < galaxyCoordinates.size(); i++) {
+                for (var j = i + 1; j < galaxyCoordinates.size(); j++) {
+                    galaxyPairs.add(new GalaxyPair(galaxyCoordinates.get(i), galaxyCoordinates.get(j)));
+                }
+            }
+            return galaxyPairs;
+        }
+
+        public long sumDistanceBetweenUniqueGalaxyPairs() {
+            return galaxyPairs.stream().mapToLong(GalaxyPair::distance).sum();
+        }
+
+        class GalaxyPair {
+            TreeSet<Point> coordinates = new TreeSet<>();
+
+            GalaxyPair(Point galaxyA, Point galaxyB) {
+                coordinates.add(galaxyA);
+                coordinates.add(galaxyB);
+            }
+
+            public long distance() {
+                if (coordinates.isEmpty() || coordinates.size() == 1) {
+                    return 0;
+                }
+                var minY = Math.min(coordinates.first().y(), coordinates.last().y());
+                var maxY = Math.max(coordinates.first().y(), coordinates.last().y());
+                var minX = Math.min(coordinates.first().x(), coordinates.last().x());
+                var maxX = Math.max(coordinates.first().x(), coordinates.last().x());
+
+                var expansionColumnsTraversed = expansionColumns.stream()
+                        .filter(x -> x > minX && x < maxX)
+                        .toList().size();
+                var expansionRowsTraversed = expansionRows.stream()
+                        .filter(y -> y > minY && y < maxY)
+                        .toList().size();
+
+                var rawDistance = coordinates.first().gridDistanceTo(coordinates.last());
+                var totalExpansions = expansionColumnsTraversed + expansionRowsTraversed;
+                var expandedDistance = (EXPANSION_DISTANCE * totalExpansions) - totalExpansions;
+                var totalDistance = rawDistance + expandedDistance;
+
+                out.printf("Galaxy pair %s has raw distance %d and crosses %d horizontal and %d vertical expansions (%d total) for %d additional distance for a total of %d\n",
+                        this, rawDistance, expansionColumnsTraversed, expansionRowsTraversed, totalExpansions, expandedDistance, totalDistance);
+                return totalDistance;
+            }
+
+            @Override
+            public String toString() {
+                final StringBuilder sb = new StringBuilder("GalaxyPair{");
+                sb.append("coordinates=").append(coordinates);
+                sb.append('}');
+                return sb.toString();
+            }
         }
     }
 
